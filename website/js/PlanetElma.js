230c131a47ad3cf6fbeae4c9f799fbbc6d68a3ca
elmaaquino <elmaaquino@hotmail.co.uk>
2015-02-21 21:27:19 +0000

    added test content images

--- website/js/PlanetElma.js
+++ website/js/PlanetElma.js
@@ -1,44 +1,25 @@
+var DataManager, SelectionList;
 
-//yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+
 
 function PlanetElma(loader) {
 
-    this.loader = loader;
+    this.dataManager = new DataManager();
+
 	this.selectedMenu=0;
 	//this.selection = [];
 	this.selectionList = new SelectionList();
 	this.subMenuList = new SelectionList();
 	
-	this.imageUrl='/images/testcontent/';
-	this.configUrl='/config/';
 }
 
 
 PlanetElma.prototype.LoadImages = function () {
     var that =this;
-    
-// 	$.getJSON( "/imagebyparentid?pid=0", function( data ) {
-// 	   that.displayImages(data);		 
-// 	});
 
-    $.getJSON( "/config/images.js", function( data ) {
-
-        var idx =0;
-        var parents = [];
-        
-        while(idx < data.length){
-            if(data[idx].parentCId ==0){
-                parents.push(data[idx]);
-            }
-            idx++;
-        }
-
-        data = parents;
-
-        that.displayImages(data);	
-         
+    this.dataManager.GetImagesByParentId(0, function(data){
+        that.displayImages(data);
     });
-
 
 	$('body').on("click", ".frame", $.proxy(function (e) { 
         var id = e.target.attributes['data-id'].value;
@@ -51,7 +32,7 @@ PlanetElma.prototype.LoadImages = function () {
 
 PlanetElma.prototype.displayImages = function (data) {
     var that =this;
-    var idx = 0;
+   
     var newInner = "";
     
     $.each( data, function( key, val ) {		
@@ -67,24 +48,12 @@ PlanetElma.prototype.SelectImage = function (id) {
 
     var that = this;
 
-    $.getJSON( "/config/images.js", function( data ) {
-
-        var idx =0;
-        var parents = [];
-        
-        while(idx < data.length){
-            if(data[idx].parentCId ==id){
-                parents.push(data[idx]);
-            }
-            idx++;
-        }
-
-        data = parents;
-
+    this.dataManager.GetImagesByParentId(id, function(data){
         that.displayImages(data);	
-        
         that.LoadMenus(1);	
     });
+    
+    
     
     // get child images from parentid
 };
@@ -97,23 +66,12 @@ PlanetElma.prototype.LoadMenus = function (parentId) {
     {
         parentId =0;
         
-        $.getJSON( "/config/menus.js", function( data ) {
-            
-            var idx =0;
-            var parents = [];
-            
-            while(idx < data.length){
-                if(data[idx].parentMenuId ==parentId){
-                    parents.push(data[idx]);
-                }
-                idx++;
-            }
-            
-            data = parents;
-            
+        this.dataManager.GetMenusByParentId(parentId, function(data){
             that.selectionList.populateList(data, that.SelectMenu, that, 'menu-container');
         });
     }
+     
+
 };
 
 PlanetElma.prototype.SelectMenu = function (evt) {
@@ -121,22 +79,8 @@ PlanetElma.prototype.SelectMenu = function (evt) {
     var that = this;
     console.log('select menu:'+evt);
 
-     $.getJSON( "/config/images.js", function( data ) {
-
-        var idx =0;
-        var parents = [];
-        
-        while(idx < data.length){
-            if(data[idx].menuId ==evt){
-                parents.push(data[idx]);
-            }
-            idx++;
-        }
-
-        data = parents;
-
-        that.displayImages(data);	
-         
+    this.dataManager.GetImagesByMenuId(evt, function(data){
+        that.displayImages(data);
     });
-    
+     
 };
