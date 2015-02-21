80fa366119c8b4e1146cbb55c30987baa5f4bebd
elmaaquino <elmaaquino@hotmail.co.uk>
2015-02-21 19:28:08 +0000

    rebirth

---
+++ website/config/images.js
@@ -0,0 +1,254 @@
+[
+  {
+    "_id": "536eac26e4b0e36cd2b8e9e4",
+    "cId": 1,
+    "description": "cats stuff",
+    "imageName": "cat.jpg",
+    "menuId": 1,
+    "parentCId": 0,
+    "responseId": 0,
+    "lImage": {
+      "name": "cat.jpg",
+      "x": 300,
+      "y": 600
+    },
+    "contentDim": {
+      "x": 300,
+      "y": 600
+    }
+  },
+  {
+    "_id": "536fda64e4b0e36cd2b8f015",
+    "cId": 2,
+    "description": "rabbit stuff",
+    "imageName": "rabbit.jpg",
+    "menuId": 1,
+    "parentCId": 0,
+    "responseId": 0,
+    "lImage": {
+      "name": "rabbit.jpg",
+      "x": 300,
+      "y": 600
+    },
+    "contentDim": {
+      "x": 300,
+      "y": 600
+    }
+  },
+  {
+    "_id": "536fd8d3e4b0e36cd2b8f00c",
+    "cId": 3,
+    "description": "squirrel stuff",
+    "imageName": "squirrel.jpg",
+    "menuId": 1,
+    "parentCId": 0,
+    "responseId": 0,
+    "lImage": {
+      "name": "squirrel.jpg",
+      "x": 300,
+      "y": 600
+    },
+    "contentDim": {
+      "x": 300,
+      "y": 600
+    }
+  },
+  {
+    "_id": "53769228e4b07340cb19dbae",
+    "cId": 4,
+    "description": "chicken stuff",
+    "imageName": "chicken.jpg",
+    "menuId": 2,
+    "parentCId": 0,
+    "responseId": 0,
+    "lImage": {
+      "name": "chicken.jpg",
+      "x": 300,
+      "y": 600
+    },
+    "contentDim": {
+      "x": 300,
+      "y": 600
+    }
+  },
+  {
+    "_id": "53e67652e4b042a207c66827",
+    "cId": 5,
+    "description": "fishy stuff",
+    "imageName": "fish.JPG",
+    "menuId": 3,
+    "parentCId": 0,
+    "responseId": 0,
+    "lImage": {
+      "name": "fish.JPG",
+      "x": 300,
+      "y": 600
+    },
+    "contentDim": {
+      "x": 300,
+      "y": 600
+    }
+  },
+  {
+    "_id": "53e6c5c5e4b042a207c66b34",
+    "cId": 6,
+    "description": "black cat stuff",
+    "imageName": "blackcat.JPG",
+    "menuId": 1,
+    "parentCId": 1,
+    "responseId": 0,
+    "lImage": {
+      "name": "blackcat.JPG",
+      "x": 300,
+      "y": 600
+    },
+    "contentDim": {
+      "x": 300,
+      "y": 600
+    }
+  },
+  {
+    "_id": "53e6c6e0e4b042a207c66b37",
+    "cId": 7,
+    "description": "black and white cat stuff",
+    "imageName": "blackwhitecat.JPG",
+    "menuId": 1,
+    "parentCId": 1,
+    "responseId": 0,
+    "lImage": {
+      "name": "blackwhitecat.JPG",
+      "x": 300,
+      "y": 600
+    },
+    "contentDim": {
+      "x": 300,
+      "y": 600
+    }
+  },
+  {
+    "_id": "53e6c76be4b042a207c66b39",
+    "cId": 8,
+    "description": "white cat stuff",
+    "imageName": "wildcat.jpg",
+    "menuId": 1,
+    "parentCId": 1,
+    "responseId": 0,
+    "lImage": {
+      "name": "wildcat.jpg",
+      "x": 300,
+      "y": 600
+    },
+    "contentDim": {
+      "x": 300,
+      "y": 600
+    }
+  },
+  {
+    "_id": "53e6c7aee4b042a207c66b3a",
+    "cId": 9,
+    "description": "kitten cat stuff",
+    "imageName": "kitten.jpg",
+    "menuId": 1,
+    "parentCId": 1,
+    "responseId": 0,
+    "lImage": {
+      "name": "kitten.jpg",
+      "x": 300,
+      "y": 600
+    },
+    "contentDim": {
+      "x": 300,
+      "y": 600
+    }
+  },
+  {
+    "_id": "53e6c8f1e4b042a207c66b3c",
+    "cId": 10,
+    "description": "bunny stuff",
+    "imageName": "bunny.jpg",
+    "menuId": 1,
+    "parentCId": 2,
+    "responseId": 0,
+    "lImage": {
+      "name": "bunny.jpg",
+      "x": 300,
+      "y": 600
+    },
+    "contentDim": {
+      "x": 300,
+      "y": 600
+    }
+  },
+  {
+    "_id": "53e6ccaee4b042a207c66b55",
+    "cId": 11,
+    "description": "baby-squirrel stuff",
+    "imageName": "baby-squirrel.jpg",
+    "menuId": 1,
+    "parentCId": 3,
+    "responseId": 0,
+    "lImage": {
+      "name": "baby-squirrel.jpg",
+      "x": 300,
+      "y": 600
+    },
+    "contentDim": {
+      "x": 300,
+      "y": 600
+    }
+  },
+  {
+    "_id": "53e6cd58e4b042a207c66b56",
+    "cId": 12,
+    "description": "chick stuff",
+    "imageName": "chick.jpg",
+    "menuId": 2,
+    "parentCId": 4,
+    "responseId": 0,
+    "lImage": {
+      "name": "chick.jpg",
+      "x": 300,
+      "y": 600
+    },
+    "contentDim": {
+      "x": 300,
+      "y": 600
+    }
+  },
+  {
+    "_id": "53e6cdcde4b042a207c66b5a",
+    "cId": 13,
+    "description": "fishy stuff",
+    "imageName": "babyfish.jpg",
+    "menuId": 3,
+    "parentCId": 5,
+    "responseId": 0,
+    "lImage": {
+      "name": "babyfish.jpg",
+      "x": 300,
+      "y": 600
+    },
+    "contentDim": {
+      "x": 300,
+      "y": 600
+    }
+  },
+  {
+    "_id": "53e6ce3de4b042a207c66b5c",
+    "cId": 14,
+    "description": "bantam stuff",
+    "imageName": "bantam.JPG",
+    "menuId": 2,
+    "parentCId": 4,
+    "responseId": 0,
+    "lImage": {
+      "name": "bantam.JPG",
+      "x": 300,
+      "y": 600
+    },
+    "contentDim": {
+      "x": 300,
+      "y": 600
+    }
+  }
+]
