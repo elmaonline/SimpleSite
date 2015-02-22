08d3dbc11b2cd8d3c82aec9ec61bf0e7fd963bdd
elmaaquino <elmaaquino@hotmail.co.uk>
2015-02-22 00:30:17 +0000

    misc changes

--- website/js/main.js
+++ website/js/main.js
@@ -1,9 +1,29 @@
 ﻿$(document).ready(function() {
 
- 
-    var planetElma = new PlanetElma(new DataManager());
+    var qs = (function(a) {
+        if (a == "") return {};
+        var b = {};
+        for (var i = 0; i < a.length; ++i)
+        {
+            var p=a[i].split('=', 2);
+            if (p.length == 1)
+                b[p[0]] = "";
+            else
+                b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+        }
+        return b;
+    })(window.location.search.substr(1).split('&'));
+    
+//With an URL like ?topic=123&name=query+string, the following will return:
+
+  
+  
+    var planetElma = new PlanetElma(new DataManager(),qs["menu"],qs["img"]);
 
 	planetElma.LoadMenus();
 	
 //    planetElma.LoadImages();
+
+
+
 });
